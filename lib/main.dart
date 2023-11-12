1a7e28daa301e09c956578f471e7a15a4e28342a
Anish Deshpande <anishmdeshpande@gmail.com>
2023-11-12 13:31:44 +0530

    Login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
-import 'pages/homepage.dart';
-import 'pages/loginpage.dart';
-import 'pages/signup.dart';
+import 'package:investment_manager/pages/homepage.dart';
+import 'package:investment_manager/pages/loginpage.dart';
+import 'package:investment_manager/pages/signup.dart';
 
 void main() => runApp(MaterialApp(
-      theme: ThemeData(
-        useMaterial3: true,
-        scaffoldBackgroundColor: Colors.white,
-      ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => LoginPage(),
-        '/signup': (context) => SignUpPage(),
-        '/home': (context) => HomePage(),
-
-
-
-      },
-  )
+  theme: ThemeData(
+    useMaterial3: true,
+    scaffoldBackgroundColor: Colors.black,
+  ),
+  initialRoute: '/',
+  routes: {
+    '/': (context) => LoginPage(),
+    '/signup': (context) => SignUpPage(),
+    '/home': (context) => HomePage(),
+  },
+)
 );
-
--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -20,10 +20,10 @@ class _LoginPageState extends State<LoginPage> {
                 height: 30.0,
               ),
               Text("App Name",
-              style: TextStyle(
-                fontSize: 28.0,
-                color: Colors.black,
-              )),
+                  style: TextStyle(
+                    fontSize: 28.0,
+                    color: Color(0xFFF9FAF8),
+                  )),
               SizedBox(
                 height: 40.0,
               ),
@@ -31,35 +31,67 @@ class _LoginPageState extends State<LoginPage> {
                 height: 100.0,
                 width: 100.0,
                 child: Image(image: AssetImage('assets/loginlogo.png'),
-                fit: BoxFit.cover),
+                  fit: BoxFit.cover,
+                  color: Color(0xFFF9FAF8),
+                ),
               ),
               SizedBox(
                 height: 30.0,
               ),
               Padding(
-                padding: const EdgeInsets.fromLTRB(16.0, 32.0, 16.0, 0.0),
+                padding: const EdgeInsets.fromLTRB(40.0, 16.0, 40.0, 0.0),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    TextFormField(
+                    TextField(
                       decoration: InputDecoration(
+                        iconColor: Colors.white,
                         labelText: ("Email/Phone"),
+                        labelStyle: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFFF9FAF8)
+                        ),
                         hintText: ("Enter email address/phone number"),
                         hintStyle: TextStyle(
-                          color: Colors.grey[700],
+                          color: Color(0xFFF9FAF8),
+                        ),
+                        border: OutlineInputBorder(),
+                        fillColor: Color(0xFF313131),
+                        filled: true,
+                        focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                        color: Color(0xFFF0E68C),
+                          ),
                         ),
                       ),
+                      style: TextStyle(color: Color(0xFFF9FAF8)),
                     ),
-                    TextFormField(
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextField(
                       obscureText: true,
                       decoration: InputDecoration(
                         label: Text("Password"),
+                        labelStyle: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFFF9FAF8),
+                        ),
                         hintText: ("Enter Password"),
                         hintStyle: TextStyle(
-                          color: Colors.grey[700],
+                          color: Color(0xFFF9FAF8),
+                        ),
+                        border: OutlineInputBorder(),
+                        fillColor: Color(0xFF313131),
+                        filled: true,
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0xFFF0E68C)
+                          ),
                         ),
                       ),
+                      style: TextStyle(color: Color(0xFFF9FAF8)),
                     ),
-                    //
                   ],
                 ),
               ),
@@ -67,34 +99,61 @@ class _LoginPageState extends State<LoginPage> {
                 height: 10.0,
               ),
               TextButton(
-                  onPressed: (){},
-                  child: Text("Forgot Password"),
+                onPressed: (){},
+                child: Text("Forgot Password?"),
+                style: ButtonStyle(
+                  foregroundColor: MaterialStatePropertyAll(Color(0xFFF0E68C)),
+                ),
               ),
               SizedBox(
-                height: 15.0,
+                height: 10.0,
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(onPressed: (){
-                    Navigator.pushNamed(context, '/home');
-                  },//make pushReplacementNamed later
-                      child: Text("Login"),
+              ElevatedButton(onPressed: (){
+                Navigator.pushNamed(context, '/home');
+              },//make pushReplacementNamed later
+                child: Text("Login",
+                  style: TextStyle(
+                    fontSize: 20,
                   ),
-                  SizedBox(
-                    width: 15.0,
+                ),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Color(0xFFF0E68C)),
+                  foregroundColor: MaterialStatePropertyAll(Colors.grey[900]),
+                  fixedSize: MaterialStatePropertyAll(Size(330, 60)),
+                  shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0), //
+                    ),
                   ),
-                  ElevatedButton(onPressed: (){
-                    Navigator.pushReplacementNamed(context, '/signup');
-                  },
-                      child: Text("Sign Up"),
-                  ),
-                ],
+                ),
               ),
-             TextButton.icon(onPressed: (){},
-                 icon: Icon(Icons.g_mobiledata), label: Text("Login with Google"),
-             )
-
+              SizedBox(
+                  height: 30.0
+              ),
+              ElevatedButton.icon(onPressed: (){},
+                icon: Icon(Icons.g_mobiledata), label: Text("Login with Google"),
+                style: ButtonStyle(
+                  fixedSize: MaterialStatePropertyAll(Size(330, 50)),
+                  backgroundColor: MaterialStatePropertyAll(Colors.black),
+                  foregroundColor: MaterialStatePropertyAll(Color(0xFFF0E68C)),
+                  shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                    side: BorderSide(color: Colors.yellow, width: 2),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              TextButton(
+                onPressed: (){
+                  Navigator.pushNamed(context, '/home');
+                },
+                child: Text("New Here? Sign Up"),
+                style: ButtonStyle(
+                  foregroundColor: MaterialStatePropertyAll(Color(0xFFF0E68C)),
+                ),
+              ),
             ],
           ),
         ),
